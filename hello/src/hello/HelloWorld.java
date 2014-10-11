363764fc67d8c911678a9a97ced5efdad5c2a9ec
bitgnu <bitgnu@163.com>
2014-10-11 12:00:27 +0800

    Update HelloWorld.java

--- hello/src/hello/HelloWorld.java
+++ hello/src/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!世界");
+		System.out.println("Hello World!");
 		System.out.println("Hello World! the new user Win!");
 		System.out.println("Oh,what a pity!");
 	}
