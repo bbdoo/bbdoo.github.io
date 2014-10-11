edd497c531bdc7c01466de084c89367ae175f400
bitgnu <bitgnu@163.com>
2014-10-11 12:00:49 +0800

    Update HelloWorld.java

--- hello/src/hello/HelloWorld.java
+++ hello/src/hello/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Hello World! the new user Win!");
+		System.out.println("Hello World! The new user Win!");
 		System.out.println("Oh,what a pity!");
 	}
 
